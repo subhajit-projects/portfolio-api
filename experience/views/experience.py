f65c55bf24ba43d80d5ff57b2dfd0dfe0c67363d
subhajit-projects <subhajitdey.5198@gmail.com>
2024-01-24 09:22:40 +0530

    Add new experience api start working

--- experience/views/experience.py
+++ experience/views/experience.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from datetime import date, datetime
 from .. models import *
 from .. serializer import *
 
@@ -12,5 +13,27 @@ class experience_api(APIView):
                 "data": experienceSerializer(get_all_data, many=True).data
             }
             return Response(data=return_object, status=200)
+        except Exception as e:
+            print (e)
+
+    def post(self, request):
+        try:
+            print(request.data)
+            request_data = experienceSerializer(data=request.data)
+            print (request_data.is_valid())
+            print(request_data.data.get('designation', None))
+            # request_data['']
+            print (date.today())
+            print(request_data.data.get('designation', None))
+            print (datetime.strptime(request_data.data.get('work_end', None), "%Y-%m-%d")) #%d-%m-%Y"
+            get_all_data = experience.objects.all()
+            return_object = {
+                "status": "success",
+                "data": experienceSerializer(get_all_data, many=True).data
+            }
+            return Response(data=return_object, status=200)
+        
+        except ValueError:
+            print("Error ")
         except Exception as e:
             print (e)
