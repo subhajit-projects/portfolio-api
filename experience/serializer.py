88772c02951cbf9461d452442728f63e96c565e9
subhajit-projects <subhajitdey.5198@gmail.com>
2024-01-28 16:34:48 +0530

    Rename model field for uesr view

--- experience/serializer.py
+++ experience/serializer.py
@@ -4,6 +4,17 @@ from rest_framework import serializers
 from .models import experience
 
 class experienceSerializer(serializers.ModelSerializer):
+    # start_date = serializers.SerializerMethodField('get_work_start') # To change field name
+
     class Meta:
         model = experience
+        # fields = ('designation','company_name','work_start','work_end','is_continue','what_to_do','start_date')
         exclude = ('id',)
+
+    # def get_work_start(self, obj): # To change field name
+        # return obj.work_start
+
+    def validate(self, data):
+        if data['company_name'] == None:
+            raise serializers.ValidationError('Rating has to be between 1 and 10.')
+        return data
