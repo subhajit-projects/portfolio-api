134577f838bf529008b6400f84962be697217472
subhajit-projects <subhajitdey.5198@gmail.com>
2024-05-18 17:27:02 +0530

    user login successfull, and return details

--- site_users_auth/serializers/loginserializer.py
+++ site_users_auth/serializers/loginserializer.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from site_users.models import SiteUser
+from utils.exceptions.loginexception import LoginException
+from utils.encrypt.pbkdf2sha256 import Pbkdf2Sha256
 
 class LoginFormSerializer(serializers.ModelSerializer):
     # user_name = serializers.CharField(validators=[])
@@ -14,11 +16,14 @@ class LoginFormSerializer(serializers.ModelSerializer):
     
     def validate(self, data):
         user = SiteUser.objects.filter(user_name = data.get('user_name').lower())
-        print (user.exists())
-        print (user.values('password'))
-        print (user.values('password')[0]['password'])
-        print (user.values_list('password', flat=True))
-        print (user.values().first()['password'])
+        if user.exists() == False:
+            raise LoginException("User not found.", 'user_name')
+        # print (user.values('password'))
+        # print (user.values('password')[0]['password'])
+        # print (user.values_list('password', flat=True))
+        # print (user.values().first()['password'])
+        if Pbkdf2Sha256().verify(data.get('password'), user.values().first()['password']) == False:
+            raise LoginException("Password not match.", 'password')
         if(1==1) :
             pass
         else :
--- site_users_auth/views/siteusersauth.py
+++ site_users_auth/views/siteusersauth.py
@@ -2,6 +2,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .. serializers.loginserializer import *
+from site_users.models import SiteUser
+from site_users.serializer import SiteUsersSerializer
 from utils.globalresponse import globalresponse
 from utils.exceptions import RequiredfieldException
 
@@ -9,21 +11,29 @@ from utils.exceptions import RequiredfieldException
 
 class SiteUsersAuth(APIView):
     def post(self, request):
-        # return_object = {
-        #     "status": "success",
-        #     "data": SiteUsersSerializer(get_all_data, many=True).data
-        # }
+        return_object = {
+            # "status": "success",
+            # "data": SiteUsersSerializer(get_all_data, many=True).data
+        }
 
-        request_data = LoginFormSerializer(data=request.data)
-        if request_data.is_valid(raise_exception=False):
-            pass
+        request_data = data=request.data
+        check_request_data = LoginFormSerializer(data=request_data)
+
+        if check_request_data.is_valid(raise_exception=False):
+            token = {}
+            token['access_token']=""
+            token['expire']=""
+            token['refresh_token']=""
+            return_object['token'] = token
+            user = SiteUsersSerializer(SiteUser.objects.filter(user_name=request_data.get('user_name')).first() , many=False).data
+            return_object['user'] = user
         else:
-            default_errors = request_data.errors
+            default_errors = check_request_data.errors
             field_names = []
             for field_name, field_errors in default_errors.items():
                 # print ('field_errors: '+str(field_errors))
                 field_names.append(field_name)
                 raise RequiredfieldException(str(field_errors[0]), field_name)
         
-        return_obj = globalresponse(data=[], is_success=True, status_code=200).response_data()
+        return_obj = globalresponse(data=return_object, is_success=True, status_code=200).response_data()
         return Response(data=return_obj, status=return_obj.get("status_code"))
