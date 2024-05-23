3d3fcb9eb2c7f910daf19729498692320ddb9c0a
subhajit-projects <subhajitdey.5198@gmail.com>
2024-05-23 23:48:10 +0530

    New access token

--- site_users_auth/urls.py
+++ site_users_auth/urls.py
@@ -3,5 +3,6 @@ from site_users_auth import views
 
 urlpatterns = [
     path('', views.SiteUsersAuth.as_view(), name="site_users_api"),
+    path('refresh/', views.ChangeToken.as_view(), name="site_users_api"),
     path('login-history/', views.SiteUsersAuthHistory.as_view(), name="site_users_api"),
 ]
